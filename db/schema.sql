e6c240d07ed7dc8fd9498bf14d06837c184eb7db
Christopher Lloyd <waffles@Christophers-MacBook-Pro.local>
2018-04-29 15:18:15 -0600

    db complete

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id Int( 11 ) AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR( 255) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT 0,
+	date TIMESTAMP NOT NULL,
+
+  PRIMARY KEY ( id ) 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Bob Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger In Paradise');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger There I Said It Twice');
+INSERT INTO burgers (burger_name) VALUES ('Krabby Patty');
+INSERT INTO burgers (burger_name) VALUES ('Daemon Burger');
